95987aa5cf0a61435c14531bff99b2f1f35ef748
Thalles Alves <104025889+Th3AI99la@users.noreply.github.com>
2023-01-08 02:48:57 -0300

    dark-white

---
+++ js/dark.js
@@ -0,0 +1,19 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
+    if (mode.classList.contains ('fa-moon')) {
+        mode.classList.remove ('fa-moon');
+        mode.classList.add ('fa-sun');
+    
+    form.classList.add ('dark');
+    return;
+        
+    }  
+    
+    mode.classList.add ('fa-moon');
+    mode.classList.remove ('fa-sun');
+    form.classList.remove ('dark');
+
+});
+
